e4a72fbdea0bc1c610810e872a56c62b5382cce4
Luis <lfmnovaes@gmail.com>
2021-10-19 17:25:43 -0300

    More cleanup and basic setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 function App() {
+  const createDigits = () => {
+    const numberKeys = [];
+    for (let i = 1; i < 10; i += 1) {
+      numberKeys.push(
+        <button type="button" key={i}>{i}</button>,
+      );
+    }
+    return numberKeys;
+  };
   return (
     <div className="App">
-      Hello, World!
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span>
+          0
+        </div>
+
+        <div className="operators">
+          <button type="button">*</button>
+          <button type="button">/</button>
+          <button type="button">+</button>
+          <button type="button">-</button>
+          <button type="button">=</button>
+        </div>
+
+        <div className="digits">
+          { createDigits() }
+          <button type="button">0</button>
+          <button type="button">.</button>
+        </div>
+      </div>
     </div>
   );
 }
