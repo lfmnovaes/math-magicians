d1981e772e093485babc42fd90f3b52a68c65ebd
Luis <lfmnovaes@gmail.com>
2021-10-19 19:29:09 -0300

    Fix file/folder structure and fix styling

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
-function App() {
-  const createDigits = () => {
-    const numberKeys = [];
-    for (let i = 1; i < 10; i += 1) {
-      numberKeys.push(
-        <button type="button" key={i}>{i}</button>,
-      );
-    }
-    return numberKeys;
-  };
-  return (
-    <div className="App">
-      <div className="calculator">
-        <div className="display">
-          <span>(0)</span>
-          0
-        </div>
+import React from 'react';
+import Calculator from './components/Calculator';
+import './App.css';
 
-        <div className="operators">
-          <button type="button">*</button>
-          <button type="button">/</button>
-          <button type="button">+</button>
-          <button type="button">-</button>
-          <button type="button">=</button>
-        </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        <div className="digits">
-          { createDigits() }
-          <button type="button">0</button>
-          <button type="button">.</button>
-        </div>
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,36 @@
+function Calculator() {
+  return (
+    <div className="calculator">
+      <div className="display">
+        <span>0</span>
+      </div>
+      <div className="keyboard">
+        <div className="digits">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button">3</button>
+          <button type="button">2</button>
+          <button type="button">1</button>
+          <button id="zero" type="button">0</button>
+          <button type="button">.</button>
+        </div>
+        <div className="operators">
+          <button type="button">/</button>
+          <button type="button">x</button>
+          <button type="button">-</button>
+          <button type="button">+</button>
+          <button type="button">=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
