4e204b0c5825feee51790d063cbfc5f1f40e7e90
Luis <lfmnovaes@gmail.com>
2021-10-21 05:00:19 -0300

    Add events to manipulate the logic files
    * onClickHandler event for each button
    * Set names for buttons
    * Replace division symbol

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,41 +1,60 @@
 import React from 'react';
-import CalcKey from './CalcKey';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+    };
   }
 
+  onClickHandler = (e) => {
+    this.setState((prev) => calculate(prev, e.target.name));
+  };
+
+  handleDisplay = (next, total) => {
+    if (next == null && total === null) {
+      return 0;
+    }
+    if (next === null) {
+      return total;
+    }
+    return next;
+  };
+
   render() {
+    const { total, next } = this.state;
+
     return (
       <div className="calculator">
         <div className="display">
-          <span>0</span>
+          <span>{this.handleDisplay(total, next)}</span>
         </div>
         <div className="keyboard">
           <div className="digits">
-            <CalcKey text="AC" />
-            <CalcKey text="+/-" />
-            <CalcKey text="%" />
-            <CalcKey text="7" />
-            <CalcKey text="8" />
-            <CalcKey text="9" />
-            <CalcKey text="4" />
-            <CalcKey text="5" />
-            <CalcKey text="6" />
-            <CalcKey text="1" />
-            <CalcKey text="2" />
-            <CalcKey text="3" />
-            <CalcKey text="0" />
-            <CalcKey text="." />
+            <button onClick={this.onClickHandler} type="button" name="AC">AC</button>
+            <button onClick={this.onClickHandler} type="button" name="+/-">+/-</button>
+            <button onClick={this.onClickHandler} type="button" name="%">%</button>
+            <button onClick={this.onClickHandler} type="button" name="7">7</button>
+            <button onClick={this.onClickHandler} type="button" name="8">8</button>
+            <button onClick={this.onClickHandler} type="button" name="9">9</button>
+            <button onClick={this.onClickHandler} type="button" name="4">4</button>
+            <button onClick={this.onClickHandler} type="button" name="5">5</button>
+            <button onClick={this.onClickHandler} type="button" name="6">6</button>
+            <button onClick={this.onClickHandler} type="button" name="1">1</button>
+            <button onClick={this.onClickHandler} type="button" name="2">2</button>
+            <button onClick={this.onClickHandler} type="button" name="3">3</button>
+            <button onClick={this.onClickHandler} type="button" name="0">0</button>
+            <button onClick={this.onClickHandler} type="button" name=".">.</button>
           </div>
           <div className="operators">
-            <CalcKey text="/" />
-            <CalcKey text="x" />
-            <CalcKey text="-" />
-            <CalcKey text="+" />
-            <CalcKey text="=" />
+            <button onClick={this.onClickHandler} type="button" name="÷">÷</button>
+            <button onClick={this.onClickHandler} type="button" name="x">x</button>
+            <button onClick={this.onClickHandler} type="button" name="-">-</button>
+            <button onClick={this.onClickHandler} type="button" name="+">+</button>
+            <button onClick={this.onClickHandler} type="button" name="=">=</button>
           </div>
         </div>
       </div>
